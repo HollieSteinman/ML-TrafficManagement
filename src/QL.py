9b9ea3ec592c5b56358e807f849768e20280ade8
HollieSteinman <holllleeeeeeeeee@gmail.com>
2022-06-01 23:31:18 +1000

    Adding comments & fixes

--- src/QL.py
+++ src/QL.py
@@ -8,48 +8,60 @@ from env.traffic_intersection import TrafficIntersection
 
 def learn(
         env: TrafficIntersection,
-        learning,
-        discount,
-        epsilon,
-        episodes
+        learning: float=0.1,
+        discount: float=0.99,
+        epsilon: float=1,
+        stop_decay_percent: float=0.5,
+        episodes: float=300
     ):
+    # initialise variables
     all_rewards = {'ep': [], 'avg': [], 'max': [], 'min': []}
     rewards = []
-    analyse_every = max(1, episodes / 50)
+    analyse_every = max(1, episodes / 25)
     state = env.discretise_state(env.reset())
     q_table = defaultdict(lambda: [0 for _ in range(env.action_space.n)])
     q_table[state] = [0 for _ in range(env.action_space.n)]
-    stop_decay = episodes / 2
+    stop_decay = int(episodes * stop_decay_percent)
     epsilon_decay = epsilon / stop_decay
 
+    # loop through episodes
     for e in tqdm(range(episodes)):
         done = False
         total_reward = 0
         state = env.discretise_state(env.reset())
 
+        # select actions and step
         while not done:
-            if np.random.random() > epsilon:
+            # exploit if random < 1 - epsilon
+            if np.random.random() < 1 - epsilon:
                 action = np.argmax(q_table[state])
             else:
+                # else explore
                 action = int(env.action_space.sample())
 
-            s, reward, done, info = env.step(action)
+            # step with optimal action & discretise state
+            s, reward, done, _ = env.step(action)
             next_state = env.discretise_state(s)
 
+            # add state to q table
             if next_state not in q_table:
                 q_table[next_state] = [0 for _ in range(env.action_space.n)]
             
+            # q function
             q_table[state][action] = (1-learning) * q_table[state][action] + learning*(reward +
                 discount*max(q_table[next_state]))
             
+            # set state & reward
             state = next_state
             total_reward += reward
         
-        if epsilon > 0 and episodes < stop_decay:
+        # decay epsilon
+        if epsilon > 0 and e < stop_decay:
             epsilon -= epsilon_decay
         
         rewards.append(total_reward)
 
+        # output episode & averages
         if (e + 1) % analyse_every == 0:
             average_reward = np.mean(rewards)
             all_rewards['ep'].append(e + 1)
@@ -64,17 +76,25 @@ def learn(
 
 def test(
         env: TrafficIntersection,
-        q_table
+        q_table,
+        episodes
     ):
-    state = env.discretise_state(env.reset())
-    done = False
-    avg = 0
-    while not done:
-        action = np.argmax(q_table[state])
-        s, reward, done, info = env.step(action)
-        state = env.discretise_state(s)
-        avg += reward
-    print(f"Final average reward {avg}")
+    rewards = []
+    steps = []
+    for e in tqdm(range(episodes)):
+        step = 0
+        state = env.discretise_state(env.reset())
+        done = False
+        avg = 0
+        while not done:
+            action = np.argmax(q_table[state])
+            s, reward, done, info = env.step(action)
+            state = env.discretise_state(s)
+            avg += reward
+            step += 1
+        rewards.append(avg)
+        steps.append(step)
+    print(f"Average steps: {np.average(steps)}, final average reward {np.average(rewards)}")
 
 def save(q_table):
     with open('model.csv', 'w') as f:
@@ -106,16 +126,29 @@ def plot(rewards, episodes):
     plt.grid(True)
     plt.show()
 
-env = TrafficIntersection(
-    '/Users/holliesteinman/Documents/Uni/Year4/Sem1/Machine Learning/Assignment 2/src/env/sumo/test/traffic.net.xml',
-    '/Users/holliesteinman/Documents/Uni/Year4/Sem1/Machine Learning/Assignment 2/src/env/sumo/test/traffic.rou.xml',
-    '/Users/holliesteinman/Documents/Uni/Year4/Sem1/Machine Learning/Assignment 2/src/env/sumo/test/traffic.add.xml',
-    gui=False,
-    max_dur=2500)
+# env = TrafficIntersection(
+#     '/Users/holliesteinman/Documents/Uni/Year4/Sem1/Machine Learning/Assignment 2/src/env/sumo/test/traffic.net.xml',
+#     '/Users/holliesteinman/Documents/Uni/Year4/Sem1/Machine Learning/Assignment 2/src/env/sumo/test/traffic.rou.xml',
+#     '/Users/holliesteinman/Documents/Uni/Year4/Sem1/Machine Learning/Assignment 2/src/env/sumo/test/traffic.add.xml',
+#     gui=False,
+#     max_dur=500,
+#     action_dur=6,
+#     yellow_dur=5,
+#     green_dur=10)
 
-episodes = 100
-q_table, rewards = learn(env, 0.1, 0.95, 1, episodes)
+env = TrafficIntersection(
+    '/Users/holliesteinman/Documents/Uni/Year4/Sem1/Machine Learning/Assignment 2/src/env/sumo/real/traffic.net.xml',
+    '/Users/holliesteinman/Documents/Uni/Year4/Sem1/Machine Learning/Assignment 2/src/env/sumo/real/traffic.rou.xml',
+    '/Users/holliesteinman/Documents/Uni/Year4/Sem1/Machine Learning/Assignment 2/src/env/sumo/real/traffic.add.xml',
+    gui=False,
+    max_dur=500,
+    action_dur=6,
+    yellow_dur=5,
+    green_dur=10)
+
+episodes = 1000
+q_table, rewards = learn(env, 0.1, 0.95, 1,  0.75, episodes)
 plot(rewards, episodes)
-test(env, q_table)
+test(env, q_table, 10)
 
 #q_table = load(env)
