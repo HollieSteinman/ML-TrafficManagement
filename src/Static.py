65bf90473867010bc5fe9e8cc04a954102223ac0
HollieSteinman <holllleeeeeeeeee@gmail.com>
2022-06-01 16:29:57 +1000

    Updating static

--- src/Static.py
+++ src/Static.py
@@ -6,30 +6,33 @@ from env.traffic_intersection import TrafficIntersection
 env = TrafficIntersection('/Users/holliesteinman/Documents/Uni/Year4/Sem1/Machine Learning/Assignment 2/src/env/sumo/test/traffic.net.xml', '/Users/holliesteinman/Documents/Uni/Year4/Sem1/Machine Learning/Assignment 2/src/env/sumo/test/traffic.rou.xml', '/Users/holliesteinman/Documents/Uni/Year4/Sem1/Machine Learning/Assignment 2/src/env/sumo/test/traffic.add.xml', max_dur=2500)
 env.reset()
 
-change_steps = 13
 no_actions = env.action_space.n
 rewards = []
-for e in tqdm(range(10)):
+avg_steps = []
+episodes = 10
+phase_dur = {0: 18, 1: 13, 2: 18}
+for e in tqdm(range(episodes)):
     env.reset()
     done = False
-    next_change = change_steps
     current_step = 0
+    next_change = phase_dur[current_step]
     total_reward = 0
     steps = 0
     while not done:
         # step
         if next_change == 0:
-            next_change = change_steps
             current_step += 1
             if current_step > no_actions - 1:
                 current_step = 0
+            next_change = phase_dur[current_step]
         obsv, reward, done, info = env.step(current_step)
         total_reward += reward
         next_change -= 1
         steps +=1
-    print("Episode finished after {} steps. Total reward: {}.".format(steps, total_reward))
+    print(f"Episode finished after {steps} steps. Total reward: {total_reward}.")
     rewards.append(total_reward)
-print("Average reward: {}".format(np.mean(rewards)))
+    avg_steps.append(steps)
+print(f"Average reward: {np.average(rewards)}, Average steps: {np.average(avg_steps)}")
 env.close()
 
         
